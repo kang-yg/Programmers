a543bb2d8341c15218b26ca63adec298941fe55d
Kang Young Gyu <kang.yg.dev@gmail.com>
2022-08-05 17:32:08 +0900

    Level2_Q06 Question Solving

---
+++ src/Level2_Q07.kt
@@ -0,0 +1,12 @@
+// 다리를 지나는 트럭
+fun main(args: Array<String>) {
+    val truckCrossingTheBridge: TruckCrossingTheBridge = TruckCrossingTheBridge()
+    truckCrossingTheBridge.solution(2, 10, intArrayOf(7, 4, 5, 6))
+}
+
+class TruckCrossingTheBridge {
+    fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
+        var answer = 0
+        return answer
+    }
+}
